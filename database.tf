c3df03f256e37f72b30152ad60f86c47b6344318
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-25 15:03:56 -0400

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,39 +1,40 @@
 #Secret For RDS Database Instance 
-resource "aws_secretsmanager_secret" "${var.env}_rds_secret" {
-  depends_on = ${var.env}_rds_password_random
+resource "aws_secretsmanager_secret" "rds_secret" {
+  depends_on = [random_password.rds_password_random]
   name = "${var.env}-rds-secret"
 }
 
 #Secret Value and Secret Association"
-resource "aws_secretsmanager_secret_version"  "${var.env}_rds_secret_version" {
-depends_on = ${var.env}_rds_secret
-  secret_id = aws_secretsmanager_secret.${var.env}_rds_secret.id
-  secret_string = random_password.${var.env}_rds_password_random.result
+resource "aws_secretsmanager_secret_version" "rds_secret_version" {
+depends_on = [aws_secretsmanager_secret.rds_secret]
+  secret_id = aws_secretsmanager_secret.rds_secret.id
+  secret_string = random_password.rds_password_random.result
 }
 
 #Secret Value
-resource "random_password" "${var.env}_rds_password_random" {
+resource "random_password" "rds_password_random" {
   length = 15
 }
 #RDS Instance
-resource "aws_db_instance" "${var.env}_rds_instance" {
- depends_on = ${var.env}_rds_secret_version
+resource "aws_db_instance" "rds_instance" {
+ depends_on =[ aws_secretsmanager_secret_version.rds_secret_version]
   allocated_storage    = 10
   db_name              = "devdb"
   engine               = "mysql"
   engine_version       = "8.0"
+  identifier           = "${var.env}-rds-instance"
   instance_class       = "db.t3.micro"
   username             = "devdb_admin"
   db_subnet_group_name = "
-  password             = aws_secretsmanager_secret_version.${var.env}_rds_secret_version.secret_string
+  password             = aws_secretsmanager_secret_version.rds_secret_version.secret_string
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
 #RDS Subnet Group
-resource "aws_db_subnet_group" "${var.env}_subnet_group"{
-  name = "${var.env}_subnet_group"
+resource "aws_db_subnet_group" "subnet_group"{
+  name = "subnet_group"
   subnet_ids = [publicsubnet2_id, publicsubnet1_id]
   tags = {
-    name = ${var.env}_subnet_group
+    name = ${var.env}-subnet_group
   }
 }
