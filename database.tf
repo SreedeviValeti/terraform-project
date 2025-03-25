354be74cd3d3bd92ad82177ec2a0205deaa72d56
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-25 13:09:23 -0400

    Update database.tf

--- database.tf
+++ database.tf
@@ -14,14 +14,17 @@ resource "random_password" "${var.env}_rds_password_random" {
   length = 15
 }
 
-resource "aws_db_instance" "${var.env}_rds_instance" {
+resource "aws_db_instance" "${env}_rds_instance" {
   allocated_storage    = 10
   db_name              = "devdb"
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "devdb_admin"
-  password             = random_password.${var.env}_rds_password_random.result
+  db_subnet_group_name = "
+  password             = aws_secretsmanager_secret_version.${var.env}_rds_secret_version.secret_string
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
+
+resource "aws_db_subnet_group" "{}
