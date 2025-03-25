504149b77a377f1b81f3c1629c84f2198d493dd5
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-25 07:19:46 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,14 +7,25 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidr" {
-    type = string
+    type = map(string)
+    default = {
+     dev = "172.30.1.0/24"
+     uat = "172.30.2.0/24"
+     prd = "172.30.3.0/24"
+    }
 }
 
 variable "public_subnet_az" {
-    type = string
+    type = list(string)
+    default = ["us-east-2a", "us-east-2b"]
 }
 
 variable "instance_type" {
-   type = string
+   type = map(string)
+   default = {
+    dev = "t2.nano"
+    uat = "t2.nano"
+    prd = "t2.micro"
+   }
 }
 
