cac6b3d217f8da154c0aa40ce2fe052696cd1443
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-26 07:35:04 -0400

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -3,7 +3,7 @@ resource "null_resource" "null_resource" {
  type = "ssh"
  user = "ec2-user"
  privatekey = file("erpa-practice-key.pem")
- host = 
+ host = aws_instance.public_instance.[*].public_ip
  }
  provisioner "file" {
  source = "userdata.sh"
