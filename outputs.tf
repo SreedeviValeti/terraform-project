10fe76d5c487b8a0ffcc2978f9510b5c527245fb
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-26 03:45:45 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "ig_id" {
 }
 
 output "security_group_id" {
-    value = aws_security_group.vpc_sg.id
+    value = aws_security_group.web_sg.id
 }
 output "publicsubnet_id" {
   value = aws_subnet.public_subnet[*].id
