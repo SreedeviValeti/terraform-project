6053f66d385f83f58750432edde3dd3aea95c055
Sreedevi Valeti <106773524+SreedeviValeti@users.noreply.github.com>
2025-03-26 07:26:17 -0400

    Create provisioner.tf

---
+++ provisioner.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "null_resource" {
+ connection {
+ type = "ssh"
+ user = "ec2-user"
+ privatekey = file("erpa-practice-key.pem")
+ host = 
+ }
+ provisioner "file" {
+ source = "userdata.sh"
+ destination = "/etc/rc.d/userdata.sh"
+ }
+ provisioner "remote-exec" {
+ inline = [
+ "chmod +x /etc/rc.d/userdata.sh"
+ sudo /etc/rc.d/userdata.sh
+ ]
+ }
+}
